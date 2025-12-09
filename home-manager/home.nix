445f1d435e9a1475c657885d9cfb0b1b88480100
Armyyy <ArmyHamumu@gmail.com>
2025-12-09 22:04:18 +0700

    Import programs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,7 +19,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    # ./programs
+    ./programs
   ];
 
   nixpkgs = {
@@ -61,14 +61,7 @@
     devenv
     nerd-fonts.jetbrains-mono
     libreoffice-fresh
-    aerc
-    htop
     mission-center
-    poppler-utils
-
-    # 9/Dec/2025
-    discordo
-    virtualbox
   ];
 
   # Enable home-manager and git
@@ -102,78 +95,6 @@
       enableFishIntegration = true;
     };
     nix-index-database.comma.enable = true;
-  };
-
-  programs.zed-editor = {
-    enable = true;
-    extensions = [
-      "nix"
-    ];
-    # ~/.config/zed/settings.json
-    userSettings = {
-      vim_mode = false;
-      diagnostics = {
-        inline = {
-          enabled = true;
-        };
-      };
-      terminal = {
-        font_size = 12;
-        font_family = "JetBrainsMono Nerd Font";
-        font_weight = 100;
-        line_height = {
-          custom = 1.2;
-        };
-      };
-      buffer_font_family = "JetBrainsMono Nerd Font";
-      ui_font_family = ".ZedSans";
-      inline_code_actions = true;
-      gutter = {
-        line_numbers = true;
-        runnables = false; # Disable runnable/test buttons
-        breakpoints = false; # Hide breakpoint markers
-        folds = false; # Remove fold arrows
-        min_line_number_digits = 2;
-      };
-      relative_line_numbers = "disabled";
-      project_panel = {
-        default_width = 110;
-        dock = "right";
-        entry_spacing = "standard";
-        indent_size = 10;
-      };
-      tabs = {
-        file_icons = true;
-        git_status = true;
-        show_close_button = "hidden";
-        show_diagnostics = "all";
-      };
-      ui_font_size = 14;
-      buffer_font_size = 16;
-      buffer_line_height = {
-        custom = 1.2;
-      };
-      buffer_font_features = {
-        calt = false;
-      };
-      buffer_font_weight = 200;
-      toolbar = {
-        breadcrumbs = false;
-        quick_actions = false;
-      };
-      theme = "Tokyo Night";
-      icon_theme = "Catppuccin Mocha";
-      auto_install_extensions = {
-        nix = true;
-      };
-      show_completions_on_input = true;
-      scrollbar = {
-        axes = {
-          horizontal = false;
-          vertical = true;
-        };
-      };
-    };
   };
 
   programs.eza = {
---
+++ home-manager/programs/ghostty/default.nix
@@ -0,0 +1,15 @@
+{
+  programs.ghostty = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      background = "000000";
+      background-image = "/home/army/Pictures/Castorice/the_pink_team.1.jpg";
+      background-image-opacity = 0.025;
+      background-image-fit = "cover";
+
+      selection-foreground = "cell-background";
+      selection-background = "cell-foreground";
+    };
+  };
+}
