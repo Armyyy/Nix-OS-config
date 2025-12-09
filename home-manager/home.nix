be00a4d4c061c2ec109bee3c90f7ea1acd9cda37
Armyyy <ArmyHamumu@gmail.com>
2025-12-09 10:47:05 +0700

    Add discordo, virtualBox, and config zed

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -65,6 +65,10 @@
     htop
     mission-center
     poppler-utils
+
+    # 9/Dec/2025
+    discordo
+    virtualbox
   ];
 
   # Enable home-manager and git
@@ -114,28 +118,40 @@
         };
       };
       terminal = {
+        font_size = 12;
         font_family = "JetBrainsMono Nerd Font";
+        font_weight = 100;
+        line_height = {
+          custom = 1.2;
+        };
       };
       buffer_font_family = "JetBrainsMono Nerd Font";
       ui_font_family = ".ZedSans";
       inline_code_actions = true;
       gutter = {
-        breakpoints = true;
-        min_line_number_digits = 2;
         line_numbers = true;
+        runnables = false; # Disable runnable/test buttons
+        breakpoints = false; # Hide breakpoint markers
+        folds = false; # Remove fold arrows
+        min_line_number_digits = 2;
       };
-      relative_line_numbers = "enabled";
+      relative_line_numbers = "disabled";
       project_panel = {
+        default_width = 110;
         dock = "right";
         entry_spacing = "standard";
+        indent_size = 10;
       };
       tabs = {
         file_icons = true;
+        git_status = true;
+        show_close_button = "hidden";
+        show_diagnostics = "all";
       };
       ui_font_size = 14;
       buffer_font_size = 16;
       buffer_line_height = {
-        custom = 1.4;
+        custom = 1.2;
       };
       buffer_font_features = {
         calt = false;
@@ -151,6 +167,12 @@
         nix = true;
       };
       show_completions_on_input = true;
+      scrollbar = {
+        axes = {
+          horizontal = false;
+          vertical = true;
+        };
+      };
     };
   };
 
