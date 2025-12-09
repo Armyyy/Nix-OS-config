37d8aeaa1e1302f4b7c72430b281e7e6930b0abe
Armyyy <ArmyHamumu@gmail.com>
2025-12-10 00:17:49 +0700

    save fastfetch and fish changes

--- home-manager/programs/fastfetch/default.nix
+++ home-manager/programs/fastfetch/default.nix
@@ -1,4 +1,6 @@
 {
-  programs.fastfetch.enable = true;
+  programs.fastfetch = {
+    enable = true;
+  };
   programs.fish.shellAliases.fastfetch = "fastfetch --config examples/25 --logo /home/army/Pictures/Castorice/castorice-ansi";
 }
