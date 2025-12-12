c277b85d67b73956ade1f744308b5ebc840f464b
Armyyy <ArmyHamumu@gmail.com>
2025-12-12 08:52:43 +0700

    add starship to be sorted

--- home-manager/programs/starship/default.nix
+++ home-manager/programs/starship/default.nix
@@ -1,9 +1,13 @@
+{ config, ... }:
 {
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
-    # settings = {
-
-    # };
   };
+
+  # we don't use `programs.starship.settings` because it sorts the directory substitions
+  # map, but starship requires them to be sorted.
+  #
+  # https://github.com/starship/starship/issues/5572
+  home.file.${config.programs.starship.configPath}.source = ./starship.toml;
 }
