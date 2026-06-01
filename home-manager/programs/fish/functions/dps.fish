43d0251b98dac41bbcfbd0e29944557f4a82ac59
Armyyy <ArmyHamumu@gmail.com>
2026-06-01 19:13:23 +0700

    chore(fish): minimal and reasonable column name for dps function

--- home-manager/programs/fish/functions/dps.fish
+++ home-manager/programs/fish/functions/dps.fish
@@ -1,2 +1,7 @@
-# dps - docker ps using csvlens
-docker ps -a --format '{{json .}}' | mlr --jsonl --ocsv cut -o -f ID,Names,Status,CreatedAt,Image,Ports,LocalVolumes,HealthStatus | csvlens
+argparse 'full' -- $argv
+
+if set -q _flag_full
+    docker ps -a --format '{{json .}}' | mlr --jsonl --ocsv put '$__s = ($Status =~ "^Up") ? 0 : 1' then sort -n __s -r CreatedAt then cut -x -f __s then cut -o -f ID,Names,Status,CreatedAt,Image,Ports,LocalVolumes,HealthStatus then rename Names,Name,Ports,Port | csvlens
+else
+    docker ps -a --format '{{json .}}' | mlr --jsonl --ocsv put '$__s = ($Status =~ "^Up") ? 0 : 1' then sort -n __s -r CreatedAt then cut -x -f __s then cut -o -f ID,Names,Status,Ports then rename Names,Name,Ports,Port | csvlens
+end
