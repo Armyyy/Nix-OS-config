aa492b8de4d15702e11c00afc6c64e925c16e033
Armyyy <ArmyHamumu@gmail.com>
2026-06-01 16:56:49 +0700

    feat(lazygit): add description to lg fish wrapper

--- home-manager/programs/lazygit/default.nix
+++ home-manager/programs/lazygit/default.nix
@@ -1,5 +1,17 @@
+{ lib, ... }:
 {
   programs.lazygit = {
     enable = true;
+  };
+  programs.fish.functions.lg = lib.mkForce {
+    description = "lazygit TUI, cd into opened dir on exit";
+    body = ''
+      set -x LAZYGIT_NEW_DIR_FILE ~/.lazygit/newdir
+      command lazygit $argv
+      if test -f $LAZYGIT_NEW_DIR_FILE
+        cd (cat $LAZYGIT_NEW_DIR_FILE)
+        rm -f $LAZYGIT_NEW_DIR_FILE
+      end
+    '';
   };
 }
