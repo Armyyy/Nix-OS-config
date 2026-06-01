621ef2f2cf8e4fc1c16abbfa4253c6b402e4591e
Armyyy <ArmyHamumu@gmail.com>
2026-06-01 17:11:08 +0700

    feat(fish): also show previous directory for ls

--- home-manager/programs/fish/conf.d/aliases.nix
+++ home-manager/programs/fish/conf.d/aliases.nix
@@ -1,12 +1,13 @@
 {
   # eza
-  ls = "eza --tree --level=1";
+  ls = "eza --tree --level=1 -a";
 
   # tmux
   # git
   gst = "git status";
   gaa = "git add -A";
   glo = "git log --oneline";
+  gcm = "git commit -m ";
 
   # docker
   drm = "docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)";
