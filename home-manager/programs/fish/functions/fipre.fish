74e56e80ce1bd92268f424a2198d53dd8535022c
Armyyy <ArmyHamumu@gmail.com>
2026-06-01 18:27:23 +0700

    feat(fish): --zed option for fipre

--- home-manager/programs/fish/functions/fipre.fish
+++ home-manager/programs/fish/functions/fipre.fish
@@ -1,7 +1,12 @@
 # fipre - find and preview files using fzf and bat
-argparse 'hidden' -- $argv
+argparse 'hidden' 'zed' -- $argv
 set rg_args --files
 if set -q _flag_hidden
     set rg_args $rg_args --hidden
 end
-rg $rg_args | fzf --preview 'bat --color=always {}'
+set selected (rg $rg_args | fzf --preview 'bat --color=always {}')
+if test -n "$selected"
+    if set -q _flag_zed
+        zeditor $selected
+    end
+end
