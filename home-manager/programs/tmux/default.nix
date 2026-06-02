44f684861c42ea0eaa0ae5644c87cb22d58fefaa
Armyyy <ArmyHamumu@gmail.com>
2026-06-03 03:12:51 +0700

    feat(tmux): window and status bar config

--- home-manager/programs/tmux/default.nix
+++ home-manager/programs/tmux/default.nix
@@ -1,8 +1,14 @@
+{ pkgs, ... }:
+let
+  cpu = pkgs.tmuxPlugins.cpu;
+in
 {
   programs.tmux = {
     enable = true;
     aggressiveResize = true;
     keyMode = "vi";
-    extraConfig = builtins.readFile ./tmux.conf;
+    extraConfig = (builtins.readFile ./tmux.conf) + ''
+      run-shell ${cpu.rtp}
+    '';
   };
 }
