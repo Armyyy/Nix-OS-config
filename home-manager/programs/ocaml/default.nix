e507a875e9c8d8fb473762d7e2479587c151bb0d
Armyyy <ArmyHamumu@gmail.com>
2026-06-25 14:25:51 +0700

    feat: add more ocaml tools

--- home-manager/programs/ocaml/default.nix
+++ home-manager/programs/ocaml/default.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.ocaml ];
+  home.packages = [
+    pkgs.ocaml
+    pkgs.ocamlPackages.utop # REPL with readline + history
+    pkgs.ocamlPackages.ocaml-lsp # editor autocomplete, type-on-hover, errors
+    pkgs.ocamlPackages.ocamlformat # auto-format, dune calls it
+    pkgs.ocamlPackages.findlib # ocamlfind, lib resolution for dune
+  ];
 }
